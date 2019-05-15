e34a63584d965804ca61028463cfbf679eef0d79
zwol3027 <zwol3027@uni.sydney.edu.au>
2019-05-15 11:51:16 +0530

    Update new.py

--- src/new.py
+++ src/new.py
@@ -1,5 +1,3 @@
-import os
-
 main_directory = os.getcwd()
 
 os.system('touch logs/cnndm.log')
@@ -8,6 +6,8 @@ os.system('touch logs/preprocess.log')
 os.system('rm -r merged_stories_tokenized; mkdir merged_stories_tokenized')
 os.system('rm -r json_data; mkdir json_data')
 os.system('rm -r bert_data; mkdir bert_data')
+os.system('rm -r results; mkdir results')
+
 
 os.chdir(main_directory +'/src')
 
@@ -16,7 +16,15 @@ os.system('python preprocess.py -mode format_to_lines_NEW -raw_path ../merged_st
 os.system ('python preprocess.py -mode format_to_bert -dataset test -raw_path ../json_data -save_path ../bert_data -oracle_mode greedy -n_cpus 1 -log_file ../logs/preprocess.log')
 
 
-os.system('python train.py -mode test -bert_data_path ../bert_small_test_data/cnndm -test_from ../pretrained_bertsum/cnndm_bertsum_classifier_best.pt -visible_gpus 0  -gpu_ranks 0 -batch_size 30000 -result_path ../results/tester -block_trigram true -report_rouge False
+os.system('python train.py -mode test -bert_data_path ../bert_data/cnndm -test_from ../pretrained_bertsum/cnndm_bertsum_classifier_best.pt -visible_gpus 0  -gpu_ranks 0 -batch_size 30000 -result_path ../results/tester -block_trigram true -report_rouge False')
 
 os.chdir(main_directory)
 
+with open('results/tester_step0.candidate', 'r') as file:
+    summary = file.read().replace('\n', '')
+    
+summary = summary.replace('<q>', '\n')
+summary = summary.replace(' ,', ',')
+summary = summary.replace(' .', '.')
+
+print(summary)
